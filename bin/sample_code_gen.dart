448561d7e6a49b215220351bb236f6b50d20bc80
Sulav <imp-sike@pop-os.localdomain>
2024-01-17 21:55:40 +0545

    Initial Setup

---
+++ bin/sample_code_gen.dart
@@ -0,0 +1,5 @@
+import 'package:sample_code_gen/sample_code_gen.dart' as sample_code_gen;
+
+void main(List<String> arguments) {
+  print('Hello world: ${sample_code_gen.calculate()}!');
+}
---
+++ test/sample_code_gen_test.dart
@@ -0,0 +1,8 @@
+import 'package:sample_code_gen/sample_code_gen.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
